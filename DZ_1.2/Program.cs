c1afce471802f48f7c9841546d50a2805fb79cf3
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 10:59:22 +0300

    in proces

---
+++ DZ_1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meaning = Convert.ToInt32(Console.ReadLine());
+            int percent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(meaning / 100 * percent);
+        }
+    }
+}
---
+++ DZ_1.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nam_1 = Convert.ToInt32(Console.ReadLine());
+            int nam_2 = Convert.ToInt32(Console.ReadLine());
+            int nam_3 = Convert.ToInt32(Console.ReadLine());
+            int nam_4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{nam_1}{nam_2}{nam_3}{nam_4}");
+
+            string name_1 = Console.ReadLine();
+            string name_2 = Console.ReadLine();
+            string name_3 = Console.ReadLine();
+            string name_4 = Console.ReadLine();
+
+            Console.WriteLine($"{name_1}{name_2}{name_3}{name_4}");
+        }
+    }
+}
