4ef459b1a48607294a632d65b800e8943271eae2
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 10:56:45 +0300

    in process

--- DZ_C-_1/Program.cs
+++ DZ_C-_1/Program.cs
@@ -10,6 +10,28 @@ namespace DZ_C__1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number from 1 to 100: ");
+            int nam = Convert.ToInt32(Console.ReadLine());
+            if (nam % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            if (nam % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            if (nam % 3 == 0 || nam % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            if (nam % 3 != 0 | nam % 5 != 0)
+            {
+                Console.WriteLine($"{nam}");
+            }
+            if (nam < 1 | nam > 100)
+            {
+                Console.WriteLine("Error!");
+            }
         }
     }
 }
