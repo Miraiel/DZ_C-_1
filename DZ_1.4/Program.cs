7dc338c6b4ec955dba36972d91abe13cc7869542
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 11:09:13 +0300

    Done

---
+++ DZ_1.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
