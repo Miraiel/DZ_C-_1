77185c4e4c7c67a33a60c1aa6e02edb4f824b07c
BlackAngel <BlackAngel@DESKTOP-KVPD74R>
2023-06-07 10:55:03 +0300

    Add project files.

---
+++ DZ_C-_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_C__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
